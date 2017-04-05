afbdcaf513c91af4cb5c51752798175857d3bd6a
Lee Thompson <leet.104@gmail.com>
2017-04-05 21:27:29 +0100

    Cleaned up code

--- Combos.fs
+++ Combos.fs
@@ -1,32 +1,32 @@
-module Combos
+namespace FSCombinatorics
 
-(*
-My F# functional version of Python's itertools.repeat()
+module Cartesian =
 
-repeat [0,1] 2
-
->>> [x+[y] for x in [[]] for y in (0,1)]
-[[0], [1]]
->>> [x+[y] for x in [[0], [1]] for y in (0,1)]
-[[0, 0], [0, 1], [1, 0], [1, 1]]
-
-*)
-
-let repeat (arg: 'a list) (repeat: int) =
-  let pools = [ for n in 1..repeat -> arg ] // pools=[[0,1]; [0,1]]
-  
-  let rec loop lls acc = 
-    match lls with
-    | [] -> acc    
-    | h :: t -> loop t [for x in h do for y in acc do yield y @ [x]]
-
-  loop pools [[]]
+  ///Python's itertools.repeat() in F#
+  ///repeat [0,1] 2
+  ///>>> [x+[y] for x in [[]] for y in (0,1)]
+  ///[[0], [1]]
+  ///>>> [x+[y] for x in [[0], [1]] for y in (0,1)]
+  ///[[0, 0], [0, 1], [1, 0], [1, 1]]
+  let repeat (arg: 'a list) (times: int) =
+    let pools = [ for n in 1..times -> arg ] // pools=[[0,1]; [0,1]]
+    
+    let rec loop lls acc = 
+      match lls with
+      | [] -> acc    
+      | h :: t -> loop t [ for x in h do 
+                           for y in acc do 
+                           yield y @ [x] ]
+    loop pools [[]]
 
 
 
-let mapcons x = List.map (fun xs -> x :: xs)
+module Compositions =
+  let private mapcons x = List.map (fun xs -> x :: xs)
 
-/// Calculate the integer compositions of argument n
-let rec intCompositions = function
-    | 1 -> [[1]]
-    | n -> [n] :: [ for x in 1..(n - 1) do yield! (mapcons (n - x)) (intCompositions x) ]
+  /// Calculate the integer compositions of argument n
+  ///TODO make tail recursive
+  let rec intCompositions = function
+      | 1 -> [[1]]
+      | n -> [n] :: [ for x in 1..(n - 1) do 
+                      yield! (mapcons (n - x)) (intCompositions x) ]
