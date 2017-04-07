a7fbba96f5ed58787f78a10832ba588c28b10265
lpt1 <leet.104@gmail.com>
2017-04-07 14:58:10 +0100

    renamed repeat to product

--- src/Combos.fs
+++ src/Combos.fs
@@ -2,13 +2,13 @@ namespace FSCombinatorics
 
 module Cartesian =
 
-  ///Python's itertools.repeat() in F#
+  ///Python's itertools.product() in F#
   ///repeat [0,1] 2
   ///>>> [x+[y] for x in [[]] for y in (0,1)]
   ///[[0], [1]]
   ///>>> [x+[y] for x in [[0], [1]] for y in (0,1)]
   ///[[0, 0], [0, 1], [1, 0], [1, 1]]
-  let repeat (arg: 'a list) (times: int) =
+  let product (arg: 'a list) (times: int) =
     let pools = [ for n in 1..times -> arg ] // pools=[[0,1]; [0,1]]
     
     let rec loop lls acc = 
